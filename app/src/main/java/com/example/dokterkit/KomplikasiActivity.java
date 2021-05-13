9a3ae65c27969ee7c16dd1b1036e61021b8eab0a
annaxatma <aatmadjaja03@student.ciputra.ac.id>
2021-05-13 15:29:43 +0900

    second add

---
+++ app/src/main/java/com/example/dokterkit/KomplikasiActivity.java
@@ -0,0 +1,14 @@
+package com.example.dokterkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KomplikasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_komplikasi);
+    }
+}
